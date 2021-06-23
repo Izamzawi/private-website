06a082e14c0ac946fdc53b88252e4f15643e845e
Zamzawi <idhamzamzawi@gmail.com>
2021-06-23 13:38:14 +0700

    Fix responsive image css

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+(function (){
+  for(x=0; x<9;x++) {
+    var right = document.createElement('div');
+    right.className = "box box-right";
+    document.getElementById('cluster-right').appendChild(right);
 
+    var left = document.createElement('div');
+    left.className = "box box-left";
+    document.getElementById('cluster-left').appendChild(left);
+    }  
+} ())
 
 var boxes = document.getElementsByClassName("box");
 var boxLeft = document.getElementsByClassName("box-left");
@@ -9,16 +19,14 @@ Array.prototype.forEach.call(boxes, (box) => {
   box.style.transform = "rotate("+Math.random()*90+"deg)";
   box.style.height = 15+(Math.random()*40) + "px";
   box.style.width = box.style.height;  
+  box.style.top = 5+(Math.random()*95) + "vh";
 })
 
 // This rule below applied each 5 box divided in left and right position
 Array.prototype.forEach.call(boxLeft, (box) => {
-  box.style.top = 0+(Math.random()*95) + "vh";
-  box.style.left = 0+(Math.random()*35) + "vw";
-
+  box.style.left = 5+(Math.random()*35) + "vw";
 })
 Array.prototype.forEach.call(boxRight, (box) => {
-  box.style.top = 5+(Math.random()*95) + "vh";
   box.style.left = 60+(Math.random()*35) + "vw";
 })
 
@@ -41,5 +49,3 @@ if(mq.matches){
 function menuHide(){
   document.getElementById("mobile-nav").checked = false;
 }
-
-
